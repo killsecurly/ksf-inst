1c60f086e1e6328e3cb0ab1a91ffcc5e28bc4bdb
KillSecurly Foundation <159317143+killsecurly@users.noreply.github.com>
2025-02-14 11:45:46 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    admin: "214214!",
+    administrator: "killsecurlyFoundation",
+    killsecurlyaccess1: "ksfacs1$9!",
+    killsecurlyaccess2: "ksfacs2*39!2%!",
+    killsecurlyaccess3: "ksfacs3!4$*1Mx*!2%*1X)!",
+    ksfsecure: "R(!(jodFS*U!JO@LSFJI*H*RY#@OAS"
   },
 };
 
